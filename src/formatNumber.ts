d79e904163d94e8828fa38c9ddc93a3300de962d
Otts <ottswang@126.com>
2023-02-27 15:35:29 +0800

    chore: 添加formatNumber、parsePrice

---
+++ src/formatNumber.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 数字格式化,整数部分每三个间隔一个逗号
+ * @param num 要格式化的数字
+ * @param sep 分隔符
+ * @returns 格式化的数字
+ */
+export const formatNumber: (num: string | number, sep?: string) => string = (
+  num,
+  sep = ", "
+) => {
+  return num
+    .toString()
+    .replace(/\d+/, (n) => n.replace(/(\d)(?=(?:\d{3})+$)/g, `$1${sep}`));
+};
