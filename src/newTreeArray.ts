f040f5a3e55617fb022ded8aa29428b751c3e7a4
Otts <ottswang@126.com>
2023-09-20 11:36:48 +0800

    release: v0.0.12 去除console.log

--- src/newTreeArray.ts
+++ src/newTreeArray.ts
@@ -2,62 +2,60 @@ import { forEach, has } from "lodash-es";
 import { getObj } from "./getObj";
 import { SetObjMapValueType } from "./setObj";
 export interface TreeItem {
-    label: string;
-    value: any;
-    children: TreeItem[];
+  label: string;
+  value: any;
+  children: TreeItem[];
 }
 
 export type TreeItemMapType = {
-    [key: string]: SetObjMapValueType;
+  [key: string]: SetObjMapValueType;
 };
 
-export const newTreeArray:
-    <T = TreeItem>(
-        treeList: any[],
-        treeItemMap?: TreeItemMapType,
-        maxLevel?: number,
-        level?: number
-    ) => T[] =
-    (
-        treeList: any,
-        treeItemMap = {
-            label: "label",
-            value: "value",
-            children: "children",
-        },
-        maxLevel = -1,
-        level = 1
-    ) => {
-        try {
-            if (maxLevel !== -1 && level > maxLevel) return [];
-            const childrenKey = has(treeItemMap,"children")?treeItemMap.children:"children";
-            const childrenMapValue = {
-                key: childrenKey,
-                getValue: (val: any[]) => {
-                    return newTreeArray(
-                        val,
-                        treeItemMap,
-                        maxLevel,
-                        level + 1
-                    );
-                },
-                defaultValue: []
-            };
-            const itemMap ={
-                label: "label",
-                value: "value",
-                ...treeItemMap,
-                children: childrenMapValue,
-            };
-            const arr: any[] = [];
-            forEach(treeList, e => {
-                const item = getObj(e, itemMap);
-                if (has(item, "children") && (!item.children || item.children.length === 0)) delete item.children;
-                arr.push(item);
-            });
-            return arr;
-        } catch (error) {
-            console.log(error);
-            return [];
-        }
+export const newTreeArray: <T = TreeItem>(
+  treeList: any[],
+  treeItemMap?: TreeItemMapType,
+  maxLevel?: number,
+  level?: number
+) => T[] = (
+  treeList: any,
+  treeItemMap = {
+    label: "label",
+    value: "value",
+    children: "children",
+  },
+  maxLevel = -1,
+  level = 1
+) => {
+  try {
+    if (maxLevel !== -1 && level > maxLevel) return [];
+    const childrenKey = has(treeItemMap, "children")
+      ? treeItemMap.children
+      : "children";
+    const childrenMapValue = {
+      key: childrenKey,
+      getValue: (val: any[]) => {
+        return newTreeArray(val, treeItemMap, maxLevel, level + 1);
+      },
+      defaultValue: [],
     };
+    const itemMap = {
+      label: "label",
+      value: "value",
+      ...treeItemMap,
+      children: childrenMapValue,
+    };
+    const arr: any[] = [];
+    forEach(treeList, (e) => {
+      const item = getObj(e, itemMap);
+      if (
+        has(item, "children") &&
+        (!item.children || item.children.length === 0)
+      )
+        delete item.children;
+      arr.push(item);
+    });
+    return arr;
+  } catch (e) {
+    return [];
+  }
+};
--- src/setObj.ts
+++ src/setObj.ts
@@ -3,23 +3,22 @@ import { defaultToUndef } from "./defaultToUndef";
 export type SetObjMapValueType =
   | string
   | {
-    key: string | string[];
-    active?: (val: any) => boolean;
-    defaultValue?: any;
-    getValue?: (val: any) => any;
-  }
+      key: string | string[];
+      active?: (val: any) => boolean;
+      defaultValue?: any;
+      getValue?: (val: any) => any;
+    }
   /**
    * 当不传key则val的值是data,此时必须有getValue函数
    */
   | {
-    active?: (val: any) => boolean;
-    getValue: (val: any) => any;
-    defaultValue?: any;
-  };
+      active?: (val: any) => boolean;
+      getValue: (val: any) => any;
+      defaultValue?: any;
+    };
 export type SetObjMapType = {
   [key: string]: SetObjMapValueType;
 };
-
 
 /**
  * 通过映射一个对象中的属性对另一个对象内的属性进行赋值操作
@@ -28,12 +27,12 @@ export type SetObjMapType = {
  * @param map 映射对象
  * @param strict 为true时是严格模式，obj的属性必须存在才能赋值
  */
-export const setObj: (obj: any, data: any, map: SetObjMapType, strict?: boolean) => void = (
-  obj,
-  data,
-  map,
-  strict = false
-) => {
+export const setObj: (
+  obj: any,
+  data: any,
+  map: SetObjMapType,
+  strict?: boolean
+) => void = (obj, data, map, strict = false) => {
   for (const key in map) {
     try {
       let dataKey: any = "";
@@ -48,24 +47,25 @@ export const setObj: (obj: any, data: any, map: SetObjMapType, strict?: boolean)
           ? get(map[key], "defaultValue")
           : "";
         const value = hasKey ? get(data, dataKey, defaultValue) : data;
-        if(has(map[key], "active")&&!get(map[key], "active", () => true)(value)) {
-          throw key+" is not activated"
+        if (
+          has(map[key], "active") &&
+          !get(map[key], "active", () => true)(value)
+        ) {
+          throw key + " is not activated";
         }
         result = has(map[key], "getValue")
           ? defaultToUndef(
-            get(map[key], "getValue", (val: any) => val)(value),
-            defaultValue,
-            val => val === undefined
-          )
+              get(map[key], "getValue", (val: any) => val)(value),
+              defaultValue,
+              (val) => val === undefined
+            )
           : hasKey
-            ? value
-            : defaultValue;
+          ? value
+          : defaultValue;
       }
       if (has(obj, key) || !strict) {
         setWith(obj, key, result, Object);
       }
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (e) {}
   }
 };
