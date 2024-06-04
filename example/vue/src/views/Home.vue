3650900312f0a3158f71c18c93f250774d210ba5
otts <ottswang@126.com>
2024-06-04 14:54:37 +0800

    feat: 测试defaultTo

--- example/vue/src/views/Home.vue
+++ example/vue/src/views/Home.vue
@@ -5,11 +5,18 @@
 </template>
 
 <script lang="ts">
+import { defaultTo,defaultToUndef } from "@otts/utils";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Home",
   mounted() {
+    const arr: Array<any> = [];
+    console.log(defaultTo(arr,()=>([])));
+    console.log(defaultTo(undefined));
+    console.log(defaultTo(NaN));
+    console.log(defaultToUndef(NaN));
+    
   },
 });
 </script>
